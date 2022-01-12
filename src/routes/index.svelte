4af85c62727eab2284b61b5ff27f87500bc9d7d5
mark <markpackham1@gmail.com>
2022-01-12 14:35:16 +0000

    refresher on data outputting
    https://www.youtube.com/watch?v=ftiTVitDbx0&list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP&index=4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'Ninja Gaming Guides'
+    const updateTitle = () => {
+      title = "Something different"
+    }
+  </script>
+  
+  <div class="index">
+    <h2>{title}</h2>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia maxime delectus, eaque, id exercitationem fugit ea ad doloribus tempore porro aut mollitia minima quisquam, vel totam animi nesciunt nihil aperiam.</p>  
+    <button on:click={updateTitle}>Change title</button>
+    <input type="text" bind:value={title}>
+  </div>
+  
+  <style>
+    .index {
+      text-align: center;
+      display: block;
+      margin: 20px auto;
+    }
+  </style>
