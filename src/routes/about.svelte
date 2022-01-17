ec25ace1bc7d42ef227e85afb2727ac79d99f331
mark <markpackham1@gmail.com>
2022-01-17 11:44:05 +0000

    overwrite page title & use $lib
    https://www.youtube.com/watch?v=shTnwJa4SRA&list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP&index=6

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Title from '$lib/title.svelte'
+</script>
+
+<Title title="Title Changed Again"/>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ex quis modi dignissimos vero eligendi porro? Consequuntur tempora praesentium non?</p>
+</div>
+
+<style>
+    .about {
+      text-align: center;
+      display: block;
+      margin: 20px auto;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,19 @@
 <script>
-    let title = 'Ninja Gaming Guides'
-    const updateTitle = () => {
-      title = "Something different"
-    }
-  </script>
-  
-  <div class="index">
-    <h2>{title}</h2>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia maxime delectus, eaque, id exercitationem fugit ea ad doloribus tempore porro aut mollitia minima quisquam, vel totam animi nesciunt nihil aperiam.</p>  
-    <button on:click={updateTitle}>Change title</button>
-    <input type="text" bind:value={title}>
-  </div>
-  
-  <style>
-    .index {
-      text-align: center;
-      display: block;
-      margin: 20px auto;
-    }
-  </style>
+  import Title from '$lib/title.svelte'
+</script>
+
+<Title />
+<div class="index">
+  <h2>Welcome</h2>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt laborum magnam deleniti recusandae, quis aperiam, architecto neque, voluptatem dicta expedita aut id voluptate omnis consectetur dolore! Quo quae perferendis vel dignissimos quasi illum aperiam dolores, id, laudantium voluptatibus fugiat accusantium impedit velit modi ratione iure qui culpa! Repellendus cupiditate aliquid quam magni. Laudantium, laboriosam quibusdam. Provident, quae incidunt! Aspernatur, vel?</p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
