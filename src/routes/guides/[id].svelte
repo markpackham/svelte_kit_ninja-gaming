996f048ca3e823bff57a1bf0abe36d95c0360e0b
mark <markpackham1@gmail.com>
2022-01-19 15:42:20 +0000

    use id based routes from local file
    https://youtu.be/0gor32MSyiA?t=324

--- src/routes/guides/[id].svelte
+++ src/routes/guides/[id].svelte
@@ -1,7 +1,8 @@
 <script context="module">
     export async function load({ page, fetch }) {
       const id = page.params.id
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      const res = await fetch(`/guides/${id}.json`)
       const guide = await res.json()
       if (res.ok) {
         return {
